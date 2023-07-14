f7376d4ac903350000850c7d2e11587dd8470f9b
DanSaurio <a22311168@uthermosillo.edu.mx>
2023-07-14 14:42:01 -0700

    Login Con validacion Funcional

---
+++ album/validarlogin.php
@@ -0,0 +1,26 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "listanime") or die("Sin conexion al servidor. Favor de revisar");
+if (!$conexion) {
+    echo "Error: No se pudo conectar a MySQL. Error " . mysqli_connect_errno() . " : ". mysqli_connect_error() . PHP_EOL;
+    die;
+}
+$queryinsert =  "SELECT * FROM usuarios WHERE mail ='{$_POST['floatingInput']}' AND password = '{$_POST['floatingPassword']}'";
+$com = mysqli_query($conexion, $queryinsert);
+    if($com->num_rows > 0 ){
+        header("location:index.html");
+    }
+    else {
+            {
+            $mensajeError = "Ocurrió un error en el proceso. Favor de verificar los datos";
+            $html = "<script>
+            function mostrarError() {
+                alert('$mensajeError');
+                window.location.href = 'login.html';
+            }
+            window.onload = mostrarError;
+        </script>";
+            echo $html;
+            exit;
+            }
+        }
+?>
